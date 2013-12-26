c490ab371889eb901e96833a6fe101db3725ee83
Dan-Tee <github@dan-t.de>
2013-12-26 11:53:08 +0100

    Made email optional

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -1,9 +1,9 @@
 class PrivateMessage < ActiveRecord::Base
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-.]+\.)+[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A\s*\z|\A[\w+\-.]+@([a-z\d\-.]+\.)+[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :message, presence: true, length: { maximum: 10000 }
 
   default_scope -> { order('created_at DESC') }
