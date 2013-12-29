f6e0dcd529581e47218b9c62dbcf002788d48a14
Dan-Tee <github@dan-t.de>
2013-12-29 02:16:38 +0100

    first test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest-rails'
+require 'minitest/rails/capybara'
+require 'minitest/colorize'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -11,5 +14,27 @@ class ActiveSupport::TestCase
   # -- they do not yet inherit this setting
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def self.prepare
+    # Add code that needs to be executed before test suite start
+  end
+  prepare
+
+  def setup
+    # Add code that need to be executed before each test
+  end
+
+  def teardown
+    # Add code that need to be executed after each test
+  end
 end
+
+#class Capybara::Rails::TestCase
+#  def setup
+#    # Code to be executed before each scenario
+#  end
+#
+#  def teardown
+#    # Code to be executed after each scenario
+#    Capybara.reset_sessions!
+#  end
+#end
