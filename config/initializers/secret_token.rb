077b62a19cd78f556bf62a104ad0ff1543344c34
Dan-Tee <github@dan-t.de>
2014-01-09 00:50:44 +0100

    new secret token deployment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,7 +9,7 @@
 require 'securerandom'
 
 def secure_token
-  token_file = Rails.root.join('.secret')
+  token_file = Rails.root.join('config/.secret')
   if File.exist?(token_file)
     File.read(token_file).chomp
   else
